0e48a5c84a3170a840a0b8e522baa997ceb33b5f
Elena <eshipkova@iMac-Dmitry.local>
2023-01-18 13:44:15 +0300

    fix: исправления по ревью-1

--- scripts/script.js
+++ scripts/script.js
@@ -3,23 +3,26 @@ const popupContainer = document.querySelector(".popup");
 const popupCloseBtn = document.querySelector(".popup__close-button");
 let profileName = document.querySelector(".profile__title");
 let profileJob = document.querySelector(".profile__subtitle");
-let nameInput = document.querySelector(".popup__input_name");
-let jobInput = document.querySelector(".popup__input_about");
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_about");
+let formElement = document.querySelector(".popup__container");
 
+// Открывает модальное окно, заполняет его поля данными из профиля
 function openPopup() {
   popupContainer.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
+// Закрывает модальное окно
 function closePopup() {
   popupContainer.classList.remove("popup_opened");
 }
 
-profileEditBtn.addEventListener("click", openPopup);
-
-let formElement = document.querySelector(".popup__container");
-
+/* Обработчик формы
+Вставляет введенные в input-ах значения в соответствующие поля профиля
+Закрывает модальное окно
+*/
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -27,6 +30,6 @@ function handleFormSubmit (evt) {
   closePopup();
 }
 
+profileEditBtn.addEventListener("click", openPopup);
 formElement.addEventListener("submit", handleFormSubmit);
-
 popupCloseBtn.addEventListener("click", closePopup);
